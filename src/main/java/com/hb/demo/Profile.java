517f57ae0b3795c0324382c27ca5275787b3f972
biao.he <254543199@qq.com>
2020-06-15 09:58:12 +0800

    第二天：springboot整合mybatis，從數據庫獲取數據並api返回

--- src/main/java/com/hb/demo/Profile.java
+++ src/main/java/com/hb/demo/Profile.java
@@ -1,12 +1,22 @@
 package com.hb.demo;
 
 public class Profile {
+    private final Integer id;
     private final String name;
     private final String paassword;
 
-    public Profile(String name, String paassword) {
+    public Profile(Integer id, String name, String paassword) {
+        this.id = id;
         this.name = name;
         this.paassword = paassword;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getPaassword() {
+        return paassword;
     }
 
     public String getName() {
--- src/main/java/com/hb/demo/ProfileController.java
+++ src/main/java/com/hb/demo/ProfileController.java
@@ -1,12 +1,19 @@
 package com.hb.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProfileController {
+
+    @Autowired
+    private ProfileMapper profileMapper;
+
     @GetMapping("/profiles")
-    public Profile profiles() {
-        return new Profile("biao.he", "123456");
+    public List<Profile> profiles() {
+        return profileMapper.getAllProfile();
     }
 }
